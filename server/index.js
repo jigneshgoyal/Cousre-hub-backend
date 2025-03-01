832137c4381b562ce7c6f61b85782ab3c943ba14
jigneshgoyal <jigneshgoyal114@gmail.com>
2025-03-01 18:14:19 +0530

    fix the CORS

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const PORT = 4000;
 database.connect();
 app.use(
   cors({
-	  origin: process.env.FRONTEND_URL,
+	  origin: "https://course-hub-frontend.onrender.com",
 	  credentials: true,
   })
 );
